03c9a9fd596924472fdb6921672686e9af2c44a9
KeremTAN <keremtan.95@hotmail.com>
2022-01-07 23:59:37 +0300

    leetcode_977

---
+++ 977_Squares_of_a_Sorted_Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public
+	vectorint sortedSquares(vectorint& nums) {
+		vectorint ret(nums.size());
+		int i = 0, j = nums.size()-1, ix = nums.size()-1, n1, n2;
+		while(i = j){
+			n1 = nums[i]  nums[i];
+			n2 = nums[j]  nums[j];
+			if(n1  n2){
+				ret[ix--] = n1;
+                i++;
+            }
+            else{
+                ret[ix--] = n2;
+                j--;
+            }
+		}
+		return ret;
+	}
+};
