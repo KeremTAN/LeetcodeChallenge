662f5a84a1b3d643c054c9ef0f6099c9ff42aff1
KeremTAN <keremtan.95@hotmail.com>
2022-01-11 03:38:20 +0300

    leetcode_1047

--- 1047_Remove_All_Adjacent_Duplicates_In_String.cpp
+++ 1047_Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -3,7 +3,7 @@ public:
     string removeDuplicates(string s) {
       string ret = "";
         for (char& c: s) {
-            if (ret.size() != 0 and ret.back() == c)
+            if (ret.size() != 0 && ret.back() == c)
                 ret.pop_back();
             else
                 ret.push_back(c);
