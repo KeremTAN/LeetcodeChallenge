55f06ca848c025fa794453b8e60200eb4bc2046f
Kerem TAN <56820099+KeremTAN@users.noreply.github.com>
2022-08-29 16:47:27 +0300

    Update 1408_String_Matching_in_an_Array.cpp

--- 1408_String_Matching_in_an_Array.cpp
+++ 1408_String_Matching_in_an_Array.cpp
@@ -1,24 +1,23 @@
-class Solution \{\
-public:\
-    vector<string> stringMatching(vector<string>& words) \{\
-        vector<string> ret;\
-        int f, l, size=words.size(), c;\
-        for(int i=0; i<size; i++)\{\
-            f = i-1;\
-            l = i+1;\
-            c=0;\
-            while(c++ < size)\{\
-                if(f > -1 && words[f--].find(words[i])!=-1)\{\
-                    ret.push_back(words[i]);\
-                    break;\
-                \}\
-                if(l < size && words[l++].find(words[i])!=-1)\{\
-                    ret.push_back(words[i]);\
-                    break;\
-                \}\
-            \}\
-        \}\
-        return ret;\
-        \
-    \}\
-\};
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ret;
+        int f, l, size=words.size(), c;
+        for(int i=0; i<size; i++){
+            f = i-1;
+            l = i+1;
+            c=0;
+            while(c++ < size) {
+                if(f > -1 && words[f--].find(words[i])!=-1) {
+                    ret.push_back(words[i]);
+                    break;
+                }
+                if(l < size && words[l++].find(words[i])!=-1) {
+                    ret.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+};
